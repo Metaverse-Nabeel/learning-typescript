a9ece035ac68e8b1a8044638399911794f344e74
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-02-15 23:58:01 +0500

    Start fabulous secret powers

--- projects/projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
+++ projects/projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
@@ -1,3 +1,6 @@
 import { characters } from "./characters";
 // Write your announceCharacter function here! ✨
 // You'll need to export it so the tests can run it.
+export function announceCharacter (rawText: string): void {
+    
+}
