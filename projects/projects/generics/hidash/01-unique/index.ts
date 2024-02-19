0460e6cfaa3aa55cc83970a861bc353ea2c5a1d7
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-02-19 13:30:22 +0500

    Generics -> Hidash -> Step-1 Unique done

--- projects/projects/generics/hidash/01-unique/index.ts
+++ projects/projects/generics/hidash/01-unique/index.ts
@@ -1,3 +1,15 @@
 // Write your unique function here! ✨
 // You'll need to export it so the tests can run it.
 // Learning Generics
+
+export function unique<Type>(...allItems: Type[][]) {
+	const found = new Set<Type>();
+
+	for (const items of allItems) {
+		for (const item of items) {
+			found.add(item);
+		}
+	}
+
+	return Array.from(found);
+}
